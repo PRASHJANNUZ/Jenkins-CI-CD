04e0386adc47576f5881d3e0c7ac6f7a0139557b
prashant302 <prashantjannu227@gmail.com>
2024-07-18 15:18:42 +0530

    tanmay added

--- src/main/java/com/Jenkins/JenkisApplication.java
+++ src/main/java/com/Jenkins/JenkisApplication.java
@@ -9,6 +9,7 @@ public class JenkisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkisApplication.class, args);
 		System.out.println("Hii Prashant Let's Start...");
+		System.out.println("Hiii Tanamy....");
 	}
 
 }
