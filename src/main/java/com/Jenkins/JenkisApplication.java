43cc28159f0d6f739d7f0a7758ba6d150210d6e6
prashant302 <prashantjannu227@gmail.com>
2024-07-18 14:30:52 +0530

    Inital commit

---
+++ src/main/java/com/Jenkins/JenkisApplication.java
@@ -0,0 +1,14 @@
+package com.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkisApplication.class, args);
+		System.out.println("Hii Prashant Let's Start...");
+	}
+
+}
