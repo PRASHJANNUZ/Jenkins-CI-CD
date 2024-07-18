c5829091a010ba4a1b8eccc14014485da4db807a
prashant302 <prashantjannu227@gmail.com>
2024-07-18 16:06:44 +0530

    added test cases

--- src/main/java/com/Jenkins/JenkisApplication.java
+++ src/main/java/com/Jenkins/JenkisApplication.java
@@ -2,9 +2,17 @@ package com.Jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkisApplication {
+
+	@GetMapping("/ping")
+	public String message() {
+		return "Wao!! Application Deployed successfully in SAP Cloud..";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkisApplication.class, args);
--- src/test/java/com/Jenkins/JenkisApplicationTests.java
+++ src/test/java/com/Jenkins/JenkisApplicationTests.java
@@ -1,13 +1,21 @@
 package com.Jenkins;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	public static Logger logger = LoggerFactory.getLogger(JenkisApplicationTests.class);
 
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
+		logger.info("Test case Executed...");
+
+	}
 }
